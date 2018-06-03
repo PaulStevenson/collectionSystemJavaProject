2afb30fdc21ee5dff219389ce1ab3fbde16a36f5
Paul <rpmstevenson@gmail.com>
2018-06-03 11:16:41 +0100

    Brand set-up

--- src/main/java/Brands/Brand.java
+++ src/main/java/Brands/Brand.java
@@ -1,6 +1,26 @@
 package Brands;
 
+import java.util.Date;
+
 public class Brand {
 
-    
+    private String name;
+    private double purchaseCost;
+    private double additionalCost;
+    private boolean favourite;
+    private double marketValue;
+//    purchaseDate needs to be added
+
+
+    public Brand(String name, double purchaseCost, double additionalCost, boolean favourite, double marketValue){
+        this.name = name;
+        this.purchaseCost = purchaseCost;
+        this.additionalCost = additionalCost;
+        this.favourite = false;
+        this.marketValue = marketValue;
+    }
+
+
+
+
 }
